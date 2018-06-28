6f2b431303d4308ebdfd42bf518c8a019ac9f77f
latty <lionel.atty@gmail.com>
2018-06-28 18:31:38 +0200

    #13: More comments/documentations.

--- src/dailycodingproblem/13/app.py
+++ src/dailycodingproblem/13/app.py
@@ -12,33 +12,45 @@ def find_longest_substring(s: str, k: int) -> str:
     :param k:
     :return:
     """
+    # longest substring (found)
     lss = ""
+    # current longest substring
     c_lss = ""
+    # current list of characters for the current longest substring
     c_c = []
-    c_nb_c = 0
 
     i = 0
     for i, c in enumerate(s):
+        # current character is in list of characters of the current substring ?
         if c in c_c:
+            # if yes, increase/update current substring
             c_lss += c
         else:
-            if c_nb_c < k:
+            # else
+            # Can we add the new character in the current substring ?
+            if len(c_c) < k:
+                # if yes: increase/updating the current substring
                 c_lss += c
-                c_c += [c]
-                c_nb_c += 1
             else:
+                # else => compare the current result (substring) & start a new substring research
+                # compare the current substring with the longest substring found as far
+                # Current substring is larger ?
                 if len(c_lss) > len(lss):
+                    # if yes: update the longest substring
                     lss = c_lss
+                # in any case => start a new substring research
+                # first element is: the last character of the previous current substring
+                c_c = [c_lss[-1]]
                 c_lss = c_lss[-1] + c
-                c_c = [c_lss[-2], c_lss[-1]]
-                c_nb_c = 2
-
-                # Early exit
+                # Early exit: at this moment, can we found a larger substring ?
                 if (len(s) - i + len(c_lss)) <= len(lss):
                     break
+            # add the new character in list of current character for substring
+            c_c += [c]
+    # perform a last comparaison for current substring
     if len(c_lss) > len(lss):
         lss = c_lss
-    print(len(s) - i)
+    # print(len(s) - i - 1)
     return lss
 
 
