63afd9bc17bbcce7ea59c33077c2771f545790cf
latty <lionel.atty@gmail.com>
2018-07-04 18:08:07 +0200

    Coding Interview Preparation: Area Overlapping Rectangles

---
+++ src/coding_interview_preparation/overlapping_rectangles/README.md
@@ -0,0 +1,21 @@
+# Problem
+
+Today's problem is asked at Amazon as well as other big companies.
+
+It's about finding the area of the overlap between two rectangles.
+
+```
+You're given 2 over-lapping rectangles on a plane.
+For each rectangle, you're given its bottom-left and top-right points.
+How would you find the area of their overlap?
+
+Input:- Rect1((2, 1), (5, 5))
+		Rect2((3, 2), (5, 7))
+
+Result:- Area of overlapping region
+```
+
+# Amazon Coding Interview - Overlapping Rectangles - Whiteboard Wednesday
+
+![https://youtube.com/watch?v=zGv3hOORxh0](https://img.youtube.com/vi/zGv3hOORxh0/0.jpg "Amazon Coding Interview - Overlapping Rectangles - Whiteboard Wednesday
+")
---
+++ src/coding_interview_preparation/overlapping_rectangles/app.py
@@ -0,0 +1,92 @@
+"""
+by Amozon (interview):
+
+You're given 2 over-lapping rectangles on a plane.
+For each rectangle, you're given its bottom-left and top-right points.
+How would you find the area of their overlap?
+
+Input:- Rect1((2, 1), (5, 5))
+        Rect2((3, 2), (5, 7))
+
+Result:- Area of overlapping region
+"""
+import attr
+
+
+@attr.s
+class Point:
+    x: float = attr.ib()
+    y: float = attr.ib()
+
+
+@attr.s
+class Interval:
+    min: float = attr.ib()
+    max: float = attr.ib()
+
+    def compute_overlapping_area(self, other: 'Interval') -> float:
+        """
+        Return length of the intersection between intervals else 0
+
+        :param other:
+        :return:
+
+        >>> Interval(0, 2).compute_overlapping_area(Interval(1, 3))
+        1
+        >>> Interval(1, 3).compute_overlapping_area(Interval(0, 2))
+        1
+        >>> Interval(1, 3).compute_overlapping_area(Interval(0, 4))
+        2
+        >>> Interval(0, 4).compute_overlapping_area(Interval(1, 3))
+        2
+        >>> Interval(0, 2).compute_overlapping_area(Interval(3, 4))
+        0
+        >>> Interval(3, 4).compute_overlapping_area(Interval(0, 2))
+        0
+        """
+        intersected_segment = min(self.max, other.max) - max(self.min, other.min)
+        return intersected_segment * (intersected_segment > 0)
+
+
+@attr.s
+# class AABox:
+class Rectangle:
+    bottom_left: Point = attr.ib()
+    top_right: Point = attr.ib()
+
+    def get_xaxis_interval(self) -> Interval:
+        return Interval(self.bottom_left.x, self.top_right.x)
+
+    def get_yaxis_interval(self) -> Interval:
+        return Interval(self.bottom_left.y, self.top_right.y)
+
+    def compute_overlapping_area(self, other: 'Rectangle') -> float:
+        """
+        Return overlapping area between 2 rectangles if overlop else 0
+        :param other:
+        :return:
+
+        >>> Rectangle(Point(2, 1), Point(5, 5)).compute_overlapping_area(Rectangle(Point(3, 2), Point(5, 7)))
+        6
+        """
+        return self.get_xaxis_interval().compute_overlapping_area(other.get_xaxis_interval()) * \
+               self.get_yaxis_interval().compute_overlapping_area(other.get_yaxis_interval())
+
+
+def main():
+    # INPUTS
+    rect1 = Rectangle(Point(2, 1), Point(5, 5))
+    rect2 = Rectangle(Point(3, 2), Point(5, 7))
+
+    # EXPECTED RESULT
+    expected_overlapping_area = 3 * 2
+
+    # COMPUTE
+    overlapping_area = rect1.compute_overlapping_area(rect2)
+
+    # TEST
+    assert overlapping_area == expected_overlapping_area
+
+
+if __name__ == '__main__':
+    main()
