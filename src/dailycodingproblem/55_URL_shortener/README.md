9b0c7b3ba6fb0c1cca29481e12f0c4b1eb3b467c
latty <yoyonel@hotmail.com>
2018-08-10 10:09:17 +0200

    Update/optimize implementation

--- src/dailycodingproblem/55_URL_shortener/README.md
+++ src/dailycodingproblem/55_URL_shortener/README.md
@@ -5,5 +5,25 @@ This problem was asked by Microsoft.
 Implement a URL shortener with the following methods:
 
 shorten(url), which shortens the url into a six-character alphanumeric string, such as zLg6wl.
-restore(short), which expands the shortened string into the original url. If no such shortened string exists, return null.
+restore(short), which expands the shortened string into the original url.
+If no such shortened string exists, return null.
+
 Hint: What if we enter the same URL twice?
+
+# Solution
+
+## How to design a tiny URL or URL shortener?
+
+https://www.geeksforgeeks.org/how-to-design-a-tiny-url-or-url-shortener/
+
+### One Simple Solution could be Hashing
+
+Use a hash function to convert long string to short string.
+In hashing, that may be collisions (2 long urls map to same short url)
+and we need a unique short url for every long url so that we can access long url back.
+
+# A Better Solution is to use the integer id stored in database
+
+and convert the integer to character string that is at most 6 characters long.
+This problem can basically seen as a base conversion problem
+where we have a 10 digit input number and we want to convert it into a 6 character long string.
--- src/dailycodingproblem/55_URL_shortener/app.py
+++ src/dailycodingproblem/55_URL_shortener/app.py
@@ -5,6 +5,23 @@ from typing import Optional
 
 
 alphanums = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+map_alphanum_to_id = {
+    **{
+        alphanum: ord(alphanum) - ord('0')
+        for alphanum in alphanums[:10]
+    },
+    **{
+        alphanum: ord(alphanum) - ord('a') + 10
+        for alphanum in alphanums[10:10+26]
+    },
+    **{
+        alphanum: ord(alphanum) - ord('A') + (10 + 26)
+        for alphanum in alphanums[10+26:]
+    },
+}
+base = len(alphanums)
+# with 6 coefficients in base-62, we can produce 56 800 235 584 values
+
 map_url_to_id = {}
 shorturls = []
 urls = []
@@ -12,8 +29,19 @@ cur_url_id = 0
 
 
 def id_to_shorturl(id_url: int) -> str:
+    """
+    Generate shorten url from id (with our bijective function using our base)
+    The shorten url will be len == 6 and all characters in ['0', ..., '9', 'a', ..., 'z', 'A', ..., 'Z']
+
+    :param id_url:
+    :return:
+
+    >>> id_to_shorturl(0)
+    '000000'
+    >>> id_to_shorturl(10)
+    '00000a'
+    """
     res = ""
-    base = len(alphanums)
     while id_url > 0:
         int_digit = id_url % base
         res += alphanums[int_digit]
@@ -22,19 +50,43 @@ def id_to_shorturl(id_url: int) -> str:
 
 
 def shorturl_to_id(short_url: str) -> int:
+    """
+
+    :param short_url: Valid short url (len == 6, all characters in ['0', ..., '9', 'a', ..., 'z', 'A', ..., 'Z']
+    :return:
+
+    >>> shorturl_to_id('000000')
+    0
+    >>> shorturl_to_id('00000a')
+    10
+    """
+    if len(short_url) != 6:
+        raise ValueError
+
     id_url = 0
-    base = len(alphanums)
-    for c in short_url:
-        if ord('0') <= ord(c) <= ord('9'):
-            id_url = id_url*base + ord(c) - ord('0')
-        if ord('a') <= ord(c) <= ord('z'):
-            id_url = id_url*base + ord(c) - ord('a') + 26
-        if ord('A') <= ord(c) <= ord('Z'):
-            id_url = id_url*base + ord(c) - ord('A') + 52
+
+    try:
+        for c in short_url:
+            id_url = id_url * base + map_alphanum_to_id[c]
+    except KeyError:
+        raise ValueError
+
     return id_url
 
 
 def shorten(url: str) -> str:
+    """
+
+    :param url:
+    :return:
+
+    >>> shorten("www.google.fr")
+    '000000'
+    >>> shorten("www.hotmail.com")
+    '000001'
+    >>> shorten("www._hotmail_.com")
+    '000001'
+    """
     global cur_url_id
 
     if url not in map_url_to_id:
@@ -51,27 +103,62 @@ def shorten(url: str) -> str:
 
 
 def restore(short_url: str) -> Optional[str]:
+    """
+
+    :param short_url:
+    :return:
+
+    >>> restore(shorten("www.google.fr"))
+    'www.google.fr'
+    >>> restore(shorten("www.hotmail.com"))
+    'www.hotmail.com'
+    >>> restore(shorten("www._hotmail_.com"))
+    'www._hotmail_.com'
+    >>> restore(shorten("www.@hotmail@.com"))
+    'www.@hotmail@.com'
+
+    >>> restore('invalid_shorten_url') is None
+    True
+
+    """
     try:
         return urls[shorturl_to_id(short_url)]
-    except IndexError:
+    except (IndexError, ValueError):
         return None
 
 
-def main():
+def tests():
     assert len(id_to_shorturl(2)) == 6
     assert id_to_shorturl(2) == f'00000{alphanums[2]}'
 
-    print(shorten("www.google.fr"))
-    print(shorten("www.google.fr/toto"))
-    print(shorten("www.google.fr/toto/tata"))
-    print(shorten("www.google.fr"))
+    test_urls = [
+        "www.google.fr",
+        "www.google.fr/toto",
+        "www.google.fr/toto/tata",
+        "www.google.fr",
+    ]
+    test_shorter_urls = [
+        shorten(url)
+        for url in test_urls
+    ]
+
+    assert all(map(lambda su: len(su) == 6, test_shorter_urls))
+
+    for url, shorten_url in zip(test_urls, shorturls):
+        assert shorten(url) == shorten_url
+
+    assert len(set(test_shorter_urls)) == len(set(test_urls))
 
     assert restore(shorten("www.google.fr")) == "www.google.fr"
     assert restore("dummy") is None
     assert restore(shorten("www.google.fr/toto/tata")) == "www.google.fr/toto/tata"
 
-    print(shorturl_to_id(shorten("www.google.fr")))
-    print(shorturl_to_id(shorten("www.google.fr/toto/tata")))
+    assert shorturl_to_id(shorten("www.google.fr")) == 0
+    assert shorturl_to_id(shorten("www.google.fr/toto/tata")) == 2
+
+
+def main():
+    tests()
 
 
 if __name__ == '__main__':
