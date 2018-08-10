d74f087b1d48bdce2f514ebb615b3a34f03e855d
latty <yoyonel@hotmail.com>
2018-08-10 18:29:17 +0200

    55: URL shortener -> add comments/refactoring

--- src/dailycodingproblem/55_URL_shortener/app.py
+++ src/dailycodingproblem/55_URL_shortener/app.py
@@ -1,8 +1,8 @@
 """
 
 """
-from typing import Optional
-
+import attr
+from typing import List, Optional
 
 alphanums = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 map_alphanum_to_id = {
@@ -12,26 +12,35 @@ map_alphanum_to_id = {
     },
     **{
         alphanum: ord(alphanum) - ord('a') + 10
-        for alphanum in alphanums[10:10+26]
+        for alphanum in alphanums[10:10 + 26]
     },
     **{
         alphanum: ord(alphanum) - ord('A') + (10 + 26)
-        for alphanum in alphanums[10+26:]
+        for alphanum in alphanums[10 + 26:]
     },
 }
 base = len(alphanums)
 # with 6 coefficients in base-62, we can produce 56 800 235 584 values
 
 map_url_to_id = {}
-shorturls = []
-urls = []
-cur_url_id = 0
+
+
+@attr.s(frozen=True, slots=True)
+class URLShortenURL:
+    __weakref__ = attr.ib(init=False, hash=False, repr=False, cmp=False)
+    #
+    url: str = attr.ib()
+    shorten_url: str = attr.ib()
+
+
+urls_shorturls = []     # type: List[URLShortenURL]
+pk_id = 0  #  primary key for url id
 
 
 def id_to_shorturl(id_url: int) -> str:
     """
     Generate shorten url from id (with our bijective function using our base)
-    The shorten url will be len == 6 and all characters in ['0', ..., '9', 'a', ..., 'z', 'A', ..., 'Z']
+    The shorten url will be len == 6 and all characters in ['0...9','a...z','A...Z']
 
     :param id_url:
     :return:
@@ -40,25 +49,34 @@ def id_to_shorturl(id_url: int) -> str:
     '000000'
     >>> id_to_shorturl(10)
     '00000a'
+    >>> id_to_shorturl(62**6 - 1)
+    'ZZZZZZ'
+    >>> id_to_shorturl(62**6 - 1 + 1)
+    '000000'
+    >>> id_to_shorturl(62**6 - 1 + 2)
+    '000001'
     """
-    res = ""
+    shorten_url = ""
+    id_url = id_url % 56800235584    # 62**6=56800235584
     while id_url > 0:
         int_digit = id_url % base
-        res += alphanums[int_digit]
+        shorten_url += alphanums[int_digit]
         id_url = id_url // base
-    return '0' * (6 - len(res)) + res
+    return '0' * (6 - len(shorten_url)) + shorten_url   # pad with '0' (neutral element) shorten url produce
 
 
 def shorturl_to_id(short_url: str) -> int:
     """
 
-    :param short_url: Valid short url (len == 6, all characters in ['0', ..., '9', 'a', ..., 'z', 'A', ..., 'Z']
+    :param short_url: Valid short url (len == 6, all characters in ['0...9','a...z','A...Z']
     :return:
 
     >>> shorturl_to_id('000000')
     0
     >>> shorturl_to_id('00000a')
     10
+    >>> shorturl_to_id('dummy_id')
+
     """
     if len(short_url) != 6:
         raise ValueError
@@ -85,21 +103,26 @@ def shorten(url: str) -> str:
     >>> shorten("www.hotmail.com")
     '000001'
     >>> shorten("www._hotmail_.com")
-    '000001'
+    '000002'
     """
-    global cur_url_id
+    global pk_id
 
+    # using map/dict to searching request url
     if url not in map_url_to_id:
-        map_url_to_id[url] = cur_url_id
-        shorturl = id_to_shorturl(cur_url_id)
-        shorturls.append(shorturl)
-        urls.append(url)
-        cur_url_id += 1
+        # if this request url not exist ->
+        # associate url to (primary key) url id
+        map_url_to_id[url] = pk_id
+        # generate shorten url from (primary key) id (using bijective function)
+        shorten_url = id_to_shorturl(pk_id)
+        # save pairing (url, shorten url)
+        urls_shorturls.append(URLShortenURL(url, shorten_url))
+        # (auto) increment (primary key) url id
+        pk_id += 1
     else:
-        id_url = map_url_to_id[url]
-        shorturl = shorturls[id_url]
+        # else retrieve information
+        shorten_url = urls_shorturls[map_url_to_id[url]].shorten_url
 
-    return shorturl
+    return shorten_url
 
 
 def restore(short_url: str) -> Optional[str]:
@@ -122,7 +145,7 @@ def restore(short_url: str) -> Optional[str]:
 
     """
     try:
-        return urls[shorturl_to_id(short_url)]
+        return urls_shorturls[shorturl_to_id(short_url)].url
     except (IndexError, ValueError):
         return None
 
@@ -144,7 +167,7 @@ def tests():
 
     assert all(map(lambda su: len(su) == 6, test_shorter_urls))
 
-    for url, shorten_url in zip(test_urls, shorturls):
+    for url, shorten_url in zip(test_urls, map(lambda usu: usu.shorten_url, urls_shorturls)):
         assert shorten(url) == shorten_url
 
     assert len(set(test_shorter_urls)) == len(set(test_urls))
